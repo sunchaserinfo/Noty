9b9956f54bf2503b7e483980db02b8f6bd3724d9
Michael Pogoda <mpogoda@lavabit.com>
2011-07-17 04:19:31 +0300

    completed, i think

--- noty.rb
+++ noty.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby19
 
-require 'rumpy'
+require './rumpy.rb'
 require 'rubygems'
 require 'tzinfo'
+require 'date'
 
 class Time
   def change(hash)
@@ -21,7 +22,7 @@ class Time
         arr[1] = h[1]
       end
     end
-    self.class.gm *arr
+    self.class.utc *arr
   end
 end
 
@@ -33,50 +34,68 @@ class Noty
     @models_path = File.dirname(__FILE__) + '/models/*.rb'
     @main_model  = :user
   end
-
   def parser_func(m)
     m.strip!
-    m = m.split(' ', 2)
-    cmd = m[0]
-    opt = m[1]
-    if opt.nil?
-      opt = ''
-    else
-      opt.strip!
-    end
-    case cmd.downcase
-    when 'help'
-      case opt
-      when 'tz'
-        opt = 'help_tz'
-      else
-        opt = 'help'
+    result = Hash.new
+    addregexp = /^((?<dwdel>\d*)(?<dwset>d|w)|((?<year>\d{2}|\d{4})-)?(?<month>\d{1,2})-(?<day>\d{1,2})|(?<day>\d{1,2})\.(?<month>\d{1,2})(\.(?<year>\d{2}|\d{4}))?)?\s*\b((?<hour>\d{1,2})(:(?<min>\d{1,2}))?\s*\b((?<ap>a|p)\.?m\.?)?|((?<hourdel>\d{1,2})h)?\s*\b((?<mindel>\d{1,2})m)?)\s*\b(?<message>.*)$/
+    if /^help\s*(.*)$/.match m.downcase do |md|
+        result[:action] = :help
+        result[:wut]    = md[1].strip
+        result[:wut]    = 'en' if result[:wut].empty?
+        true
       end
-      {:action => :show_msg, :msg => opt}
-    when 'tz'
-      if opt.empty?
-        {:action => :show_tz}
-      else
-        {:action => :set_tz, :tz => opt}
+    elsif /^tz\s*(.*)/.match m do |md|
+        result[:action] = :tz
+        result[:wut]    = md[1].strip
       end
-    when 'del'
-      {:action => :delete_record, :record => opt}
-    when 'list'
-      {:action => :show_records}
-    else
-      m = /^((((?<year>\d{4}|\d{2})-)?(?<month>\d{1,2})-(?<day>\d{1,2}))|((?<day>\d{1,2})\.(?<month>\d{1,2})(\.(?<year>\d{2}|\d{4}))?)|((?<mdnum>\d*)\s*(?<mdset>d|w)))?\s*(((?<hour>\d{1,2})((:(?<min>\d{1,2}))\s*((?<hourset>a|p)\.?m\.?)|(:(?<min>\d{1,2}))|((?<hourset>a|p)\.?m\.?)))|(((?<hourdel>\d*)h)?\s*((?<mindel>\d*)m)?))$/.match cmd
-      if m.nil?
-        {:action => nil}
-      else
-        result = Hash.new
-        m.names.each do |name|
-          result[name] = m[name]
+    elsif m == 'list' then
+      result[:action] = :list
+    elsif /^del\s*(.*)/.match m.downcase do |md|
+        result[:action] = :del
+        if md[1] == '*' then
+          result[:wut] = :all
+        else
+          result[:wut]    = md[1].to_i
         end
-        result[:action] = :add_record
-        result[:text] = opt
-        result
       end
+    elsif addregexp.match m do |md|
+        result[:action]   = :add
+        unless md[:dwset].nil?
+          result[:daydel]   = if md[:dwdel].empty? then
+                                1
+                              else
+                                md[:dwdel].to_i
+                              end
+          result[:daydel]  *= 7 if md[:dwset] == 'w'
+        end
+        result[:year]     = md[:year].to_i    unless md[:year].nil?
+        result[:month]    = md[:month].to_i   unless md[:month].nil?
+        result[:day]      = md[:day].to_i     unless md[:day].nil?
+        unless md[:hourdel].nil?
+          result[:del]    = if md[:hourdel].empty? then
+                              3600
+                            else
+                              3600 * md[:hourdel].to_i
+                            end
+        end
+        unless md[:mindel].nil?
+          result[:del]  ||= 0
+          result[:del]  += if md[:mindel].empty? then
+                                60
+                              else
+                                60 * md[:mindel].to_i
+                              end
+        end
+        result[:hour]     = md[:hour].to_i    unless md[:hour].nil?
+        result[:min]      = md[:min].to_i     unless md[:min].nil?
+        result[:ap]       = md[:ap]
+        result[:msg]      = md[:message].strip
+
+        result[:action] = nil if result[:daydel].nil? and result[:day].nil? and result[:del].nil? and result[:hour].nil? and result[:min].nil?
     end
+    end
+
+    result
   end
 
   def backend_func
@@ -91,113 +110,131 @@ class Noty
     result
   end
 
-  def do_func(user, params)
-    case params[:action]
-    when :show_msg
-      @lang[params[:msg]]
-    when :show_tz
+  def tz(user, wut)
+    if wut.empty?
       if user.timezone.nil?
         @lang['tz_not_set']
       else
         user.timezone
       end
-    when :set_tz
+    else
       begin
-        tz = TZInfo::Timezone.get params[:tz]
-        user.timezone = tz.name
+        _tz = TZInfo::Timezone.get wut
+        user.timezone = _tz.name
         user.save
         @lang['tz_set']
-      rescue
+      rescue TZInfo::InvalidTimezoneIdentifier
         @lang['wrong_tz']
       end
-    when :add_record
-      return @lang['tz_not_set'] if user.timezone.nil?
+    end
+  end
+
+  def change_date(params, current)
+    if params[:month] then
+      year_omitted = params[:year].nil?
+      params[:year] ||= current.year
+      params[:year] += 2000 if params[:year] < 100
+      wanted = current.change :year => params[:year], :month => params[:month], :day => params[:day]
+      if wanted < current then
+        if year_omitted then
+          wanted = wanted.change :year => (params[:year] + 1)
+        else
+          raise ArgumentError, 'wrong date'
+        end
+      end
+      wanted
+    elsif params[:daydel] then
+      wanted = (current.to_date + params[:daydel]).to_time
+    end
+  end
+
+  def change_time(params, current, wanted, dc)
+    if params[:hour] then
+      raise ArgumentError, 'wrong hour' if params[:hour] > 12 and params[:ap] == 'p'
+      params[:hour] = 0 if params[:hour] == 12 and params[:ap] == 'a'
+      params[:hour] += 12 if params[:hour] != 12 and params[:ap] == 'p'
+      params[:min] ||= 0
+      wanted = wanted.change :hour => params[:hour], :min => params[:min]
+      if wanted < current then
+        if date_changed then
+          raise ArgumentError, 'wrong date-time'
+        else
+          wanted = wanted.to_datetime.next.to_time
+        end
+      end
+      wanted
+    elsif params[:del] then
+      wanted += params[:del]
+    end
+  end
+
+  def add(user, params)
+    if user.timezone.nil? then
+      @lang['tz_not_set']
+    else
       tz = TZInfo::Timezone.get user.timezone
       begin
-        year_set = date_set = time_set = date_del = time_del = false
-        year_set = true unless params[:year].nil?
-        date_set = true unless params[:day].nil?
-        time_set = true unless params[:hour].nil?
-        date_del = true unless params[:mdnum].nil? and params[:mdset].nil?
-        time_del = true unless params[:hourdel].nil? and params[:mindel].nil?
-        current_dt = parsed_dt = tz.now
-        if date_set
-          #Retrieve full date
-          year = params[:year]
-          year = current_dt.year if year.nil?
-          year, month, day = year.to_i, params[:month].to_i, params[:day].to_i
-          year = 2000+year if year < 100
-          parsed_dt = parsed_dt.change :year => year, :month => month, :day => day
-          parsed_dt = parsed_dt.change :year => (year+1) if parsed_dt < current_dt and not year_set
-        elsif date_del
-          #Set up date from delay
-          n = if params[:mdnum].empty?
-                1
-              else 
-                params[:mdnum].to_i
-              end
-          n *= 7 if params[:mdset] == 'w' #weeks
-          n *= 3600*24 #FIXME!!!
-          parsed_dt += n
-        end
-        if time_set
-          #Retrieve full time
-          hour = params[:hour].to_i
-          raise if hour > 12 and params[:hourset] == 'p'
-          hour = 0 if hour == 12 and params[:hourset] == 'a' #EN style
-          hour += 12 if hour != 12 and params[:hourset] == 'p'
-          min = if params[:min].nil?
-                     0
-                   else
-                     params[:min].to_i
-                   end
-          parsed_dt = parsed_dt.change :hour => hour, :min => min
-          parsed_dt += 3600*24 if parsed_dt < current_dt and not date_set and not date_del #FIXME
-        elsif time_del
-          #Set up time from delay
-          n = 0
-          if not params[:hourdel].nil?
-            n += if params[:hourdel].empty?
-                   3600
-                 else
-                   params[:hourdel].to_i*3600
-                 end
-          end
-          if not params[:mindel].nil?
-            n += if params[:mindel].empty?
-                   60
-                 else
-                   params[:mindel].to_i*60
-                 end
-          end
-          parsed_dt += n #FIXME
-        end
-        if parsed_dt < current_dt
+        current = tz.now
+        wanted = change_date params, current
+        date_changed = if wanted.nil? then
+                         wanted = current
+                         false
+                       else
+                         true
+                       end
+
+        wanted_ = change_time params, current, wanted, date_changed
+        wanted = wanted_ unless wanted_.nil?
+
+        if wanted <= current then
           @lang['passed_date']
-        elsif parsed_dt.to_i > 2147483647 # I really don't know better way here
+        elsif wanted.to_i > 2147483647 then
           @lang['far_date']
         else
-          text = params[:text]
-          text = @lang['empty_message'] if text.is_empty?
-          parsed_dt = tz.local_to_utc parsed_dt
-          timestamp = parsed_dt.to_i
-          if user.notes.create(:text => text, :timestamp => timestamp)
-            @lang['record_added'] % parsed_dt.strftime('%Y-%m-%d %H:%M:%S')
+          params[:msg] = @lang['empty_message'] if params[:msg].empty?
+          if user.notes.create :text => params[:msg], :timestamp => (tz.local_to_utc wanted).to_i then
+            @lang['record_added'] % wanted.strftime('%Y-%m-%d %H:%M:%S')
           else
             @lang['record_add_error']
           end
         end
-      rescue
-        @lang['wrong_date']
+      rescue ArgumentError => e
+        @lang['wrong_date'] % e.message
       end
-    when :show_records
-      result = []
+    end
+  end
+
+  def do_func(user, params)
+    case params[:action]
+    when :help
+      @lang['help_' + params[:wut]]
+    when :tz
+      tz user, params[:wut]
+    when :add
+      add user, params
+    when :list
+      result = ""
       id = 0
       user.notes.each do |note|
-        id += 1
-        result << "#{id}. #{note.text}"
+        result << "#{id += 1}. #{Time.at(note.timestamp).strftime('%Y-%m-%d %H:%M:%S')} :: #{note.text}\n"
       end
-      result.join("\n")
+      result
+    when :del
+      if params[:wut] == :all then
+        user.notes.clear
+        @lang['list_cleared']
+      else
+        id = 0
+        user.notes.each do |note|
+          break note.destroy if (id += 1) == params[:wut]
+        end
+
+        if id == params[:wut] then
+          @lang['deleted']
+        else
+          @lang['not_found']
+        end
+      end
     else
       @lang['misunderstand']
     end
@@ -205,8 +242,11 @@ class Noty
 end
 
 case ARGV[0]
-when '--start'
+when 'start'
   Rumpy.start Noty
-when '--stop'
+when 'stop'
   Rumpy.stop Noty
+when 'restart'
+  Rumpy.stop Noty
+  Rumpy.start Noty
 end
